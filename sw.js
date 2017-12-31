0694ff29ea8287ae26ab8d2cfb472fca1a221e52
Shivendra Kumar <shivendra.kr.7277@gmail.com>
2017-12-31 18:42:50 +0530

    The app can work offline now.

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-var CACHE = 'offline-cache';
+var CACHE = 'offline';
 var URLS = [
+	'./',
 	'./index.html',
 	'./css/skeleton.min.css',
 	'./css/normalize.min.css',
@@ -11,35 +12,33 @@ self.addEventListener('install', event => {
 		caches.open(CACHE)
 		.then(function (cache) {
 			return cache.addAll(URLS);
-		}).then(function () {
-			return self.skipWaiting();
 		})
 	);
-	
+	console.log('installed');
 });
 
 self.addEventListener('activated', event => {
+	console.log('activated');
 });
 
 self.addEventListener('fetch', function(event) {
+	console.log('got a fetch request' + event);
 	event.respondWith(fromCache(event.request));
-	event.waitUntil(
-    	update(event.request)
-    );
+	event.waitUntil(update(event.request));
 });
 
 function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request);
+    console.log('returned from cache');
   });
 }
 
 function update(request) {
   return caches.open(CACHE).then(function (cache) {
     return fetch(request).then(function (response) {
-      return cache.put(request, response.clone()).then(function () {
-        return response;
-      });
+      return cache.put(request, response.clone());
+      console.log('update done ' + request);
     });
   });
 }
